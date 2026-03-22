e221425044abc7fc93bc1c533af633da7789fe18
Dev Raj Dev <thakurdev2672@gmail.com>
2026-03-22 23:54:43 +0530

    index file containing all components only added

---
+++ components/Content/index.tsx
@@ -0,0 +1,13 @@
+import { PartnerCompany } from "./PartnerCompany"
+import { MainContent } from "./MainContent"
+import { Testimonials } from "./Testinomials"
+
+export const Content=()=>{
+    return(
+        <>
+        <MainContent/>
+        <PartnerCompany/> 
+        <Testimonials/>
+        </>
+    )
+}
